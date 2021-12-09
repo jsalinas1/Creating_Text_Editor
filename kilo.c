8de03c5eb75b8397d257172b38eab7042031051d
Jezreel Salinas <jsalinas@cpp.edu>
2021-12-08 20:44:31 -0800

    Added quit confirmation when there's unsaved changes

--- kilo.c
+++ kilo.c
@@ -21,6 +21,7 @@
 
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -507,6 +508,8 @@ void editorMoveCursor(int key){
 }
 
 void editorProcessKeypress(){
+    static int quit_times = KILO_QUIT_TIMES;
+
     int c = editorReadKey();
 
     switch(c){
@@ -515,6 +518,12 @@ void editorProcessKeypress(){
             break;
 
         case CTRL_KEY('q'):
+            if(E.dirty && quit_times > 0){
+                editorSetStatusMessage("WARNING!! File has unsaved changes. "
+                                        "Press Ctrl Q %d more times to quit.", quit_times);
+                quit_times--;
+                return;
+            }
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
@@ -568,6 +577,7 @@ void editorProcessKeypress(){
             editorInsertChar(c);
             break;
     }
+    quit_times = KILO_QUIT_TIMES;
 }
 
 /** init **/
