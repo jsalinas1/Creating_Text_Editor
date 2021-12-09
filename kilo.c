87d8694df569face0109ff163a2811c1f1c09c3a
Jezreel Salinas <jsalinas@cpp.edu>
2021-12-08 18:58:26 -0800

    Added limit when scrolling to the right

--- kilo.c
+++ kilo.c
@@ -296,6 +296,8 @@ void editorRefreshScreen(){
 /** input **/
 
 void editorMoveCursor(int key){
+    error *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch(key){
         case ARROW_LEFT:
             if(E.cx != 0)
@@ -303,6 +305,7 @@ void editorMoveCursor(int key){
 
             break;
         case ARROW_RIGHT:
+            if(row && E.cx < row->size)
                 E.cx++;
             break;
         case ARROW_UP:
