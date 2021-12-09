137bda4dc399b18288a6404148511df25ad64473
Jezreel Salinas <jsalinas@cpp.edu>
2021-12-08 20:32:59 -0800

    When inserting chars, special char is now included

--- kilo.c
+++ kilo.c
@@ -24,6 +24,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey{
+    BACKSPACE = 127;
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
@@ -459,6 +460,10 @@ void editorProcessKeypress(){
     int c = editorReadKey();
 
     switch(c){
+        case '\r':
+        /* TODO */
+            break;
+
         case CTRL_KEY('q'):
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
@@ -472,6 +477,12 @@ void editorProcessKeypress(){
         case END_KEY:
             if(E.cy < E.numrows)
                 E.cx = E.row[E.cy].size;
+            break;
+
+        case BACKSPACE:
+        case CTRL_KEY:
+        case DEL_KEY:
+            /*TO DO*/
             break;
 
         case PAGE_UP:
@@ -495,7 +506,10 @@ void editorProcessKeypress(){
         case ARROW_RIGHT:
             editorMoveCursor(c);
             break;
-
+        case CTRL_KEY('l');
+        case '\x1b':
+            break;
+        
         default:
             editorInsertChar(c);
             break;
